b3e06b2d6e1adda38a652e1f6b4f20f8a20bf45a
danieltonel <danieltonel.xyz@gmail.com>
2022-08-28 17:44:29 +0200

    create form for editing

--- app-store/apps/mini-blog/pages/posts/[id]/edit.tsx
+++ app-store/apps/mini-blog/pages/posts/[id]/edit.tsx
@@ -1,4 +1,5 @@
 import { postSchema } from "@app-store/apps/mini-blog/api-contracts/post.schema";
+import PostForm from "@app-store/apps/mini-blog/components/PostForm";
 import Shell from "@app-store/shared/components/Shell";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -7,15 +8,11 @@ interface PostParams {
   id: string;
 }
 
-function usePostData(id: string) {
-  return useQuery(["mini-blog.posts.show"], async () => {
+export default function PostEdit({ id }: PostParams) {
+  const { data: post } = useQuery(["mini-blog.posts.show"], async () => {
     const response = await axios.get("/api/apps/mini-blog/posts/show", { params: { id } });
     return postSchema.parse(response.data);
   });
-}
-
-export default function PostEdit({ id }: PostParams) {
-  const { data: post } = usePostData(id);
 
   if (!post) return <div>No post...</div>;
 
@@ -24,8 +21,7 @@ export default function PostEdit({ id }: PostParams) {
       <div className="layout py-8">
         <h1 className="h1">Edit post</h1>
         <main>
-          <h1 className="h1">{post.title}</h1>
-          <p className="paragraph">{post.content}</p>
+          <PostForm defaultValues={post} />
         </main>
       </div>
     </Shell>
