69512345112f2e69118b7c1d1d4971bb79f5c9ca
danieltonel <danieltonel.xyz@gmail.com>
2022-09-04 23:38:08 +0200

    clean up new post page

--- app-store/apps/mini-blog/pages/posts/new.tsx
+++ app-store/apps/mini-blog/pages/posts/new.tsx
@@ -1,16 +1,7 @@
 import NewPostForm from "@app-store/apps/mini-blog/components/NewPostForm";
 import Shell from "@app-store/shared/components/Shell";
 
-interface PostParams {
-  id: string;
-}
-// TODO NEXT:
-// -> Add create page
-// -> Save and update to database
-// -> Refactor api routes /[id]/update, /[id]/delete (instead of /update /delete)
-// -> Add delete functionality
-// -> Add functionality to see posts from other users in a news feed
-export default function PostEdit({ id }: PostParams) {
+export default function PostEdit() {
   return (
     <Shell>
       <div className="layout py-8">
