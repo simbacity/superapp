ff53dca0b2bec886308962d8fd5a6c4b118662a4
SMLukwiya <sundaymorganl@gmail.com>
2022-09-13 12:20:18 +0300

    add todo filter

--- app-store/apps/todos/business-logic/Days.ts
+++ app-store/apps/todos/business-logic/Days.ts
@@ -1,6 +1,12 @@
 import dayjs from "dayjs";
+import isToday from "dayjs/plugin/isToday";
+import isTomorrow from "dayjs/plugin/isTomorrow";
 import localizedFormat from "dayjs/plugin/localizedFormat";
+import weekOfYear from "dayjs/plugin/weekOfYear";
 
 dayjs.extend(localizedFormat);
+dayjs.extend(isToday);
+dayjs.extend(isTomorrow);
+dayjs.extend(weekOfYear);
 
 export default dayjs;
--- app-store/apps/todos/pages/index.tsx
+++ app-store/apps/todos/pages/index.tsx
@@ -1,10 +1,13 @@
-import { todoListSchema } from "@app-store/apps/todos/api-contracts/todo.schema";
+import { todoListSchema, TodoResponse } from "@app-store/apps/todos/api-contracts/todo.schema";
+import dayjs from "@app-store/apps/todos/business-logic/Days";
 import TodoItem from "@app-store/apps/todos/components/TodoItem";
 import Shell from "@app-store/shared/components/Shell";
+import { XCircleIcon } from "@heroicons/react/outline";
 import { PlusCircleIcon } from "@heroicons/react/solid";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export function useGetAllTodos() {
   const getAllTodos = async () => {
@@ -15,8 +18,36 @@ export function useGetAllTodos() {
   return useQuery(["todo", "todos", "list"], () => getAllTodos());
 }
 
+const durationFilterHandler = (filter: string, data: TodoResponse[] | undefined) => {
+  if (!data) return;
+  if (filter === "today") {
+    return { data: data.filter((item) => dayjs(item.dueDate).isToday()) };
+  }
+  if (filter === "tomorrow") {
+    return { data: data.filter((item) => dayjs(item.dueDate).isTomorrow()) };
+  }
+  if (filter === "this-week") {
+    return { data: data.filter((item) => dayjs(item.dueDate).week() === dayjs().week()) };
+  }
+  if (filter === "upcoming") {
+    return { data: data.filter((item) => dayjs().isBefore(item.dueDate, "day")) };
+  }
+};
+
 export default function Index() {
   const { data: todos } = useGetAllTodos();
+  const [todoData, setTodoData] = useState<TodoResponse[] | undefined>([]);
+  const [durationFilter, setDurationFilter] = useState("");
+
+  useEffect(() => {
+    setTodoData(todos);
+  }, [todos]);
+
+  const filterHandler = (filter: string) => {
+    setDurationFilter(filter);
+    const data = durationFilterHandler(filter, todos);
+    setTodoData(data?.data);
+  };
 
   if (!todos) return <div>Loading...</div>;
 
@@ -30,33 +61,50 @@ export default function Index() {
             <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
               <div className="sticky top-0 p-4 w-full">
                 <ul className="py-2 border-b-2">
-                  <a className="link">Today</a>
+                  <a className="link" onClick={() => filterHandler("today")}>
+                    Today
+                  </a>
                 </ul>
                 <ul className="py-2 border-b-2">
-                  <a className="link">Tomorrow</a>
+                  <a className="link" onClick={() => filterHandler("tomorrow")}>
+                    Tomorrow
+                  </a>
                 </ul>
                 <ul className="py-2 border-b-2">
-                  <a className="link">This week</a>
+                  <a className="link" onClick={() => filterHandler("this-week")}>
+                    This week
+                  </a>
                 </ul>
                 <ul className="py-2 border-b-2">
-                  <a className="link">Upcoming</a>
+                  <a className="link" onClick={() => filterHandler("upcoming")}>
+                    Upcoming
+                  </a>
                 </ul>
               </div>
             </aside>
             <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 pl-1">
-              <div>
-                <button className="default-button--small">All</button>
-                <button className="default-button--small">In progress</button>
-                <button className="primary-button--small">Complete</button>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <Link href="/apps/todos/new">
+                    <PlusCircleIcon className="w-11 h-11 text-blue-800 cursor-pointer" />
+                  </Link>
+                  <h3 className="h3 px-2">Add a task</h3>
+                </div>
+                {!!durationFilter && (
+                  <div className="flex items-center bg-blue-800 p-1">
+                    <p className="text-white text-sm mr-2">{durationFilter}</p>
+                    <XCircleIcon
+                      className="w-5 h-5 text-white cursor-pointer"
+                      onClick={() => {
+                        setDurationFilter("");
+                        setTodoData(todos);
+                      }}
+                    />
+                  </div>
+                )}
               </div>
-              <div className="flex items-center">
-                <Link href="/apps/todos/new">
-                  <PlusCircleIcon className="w-11 h-11 text-blue-800 cursor-pointer" />
-                </Link>
-                <h3 className="h3 px-2">Add a task</h3>
-              </div>
               <div>
-                {todos.map((item) => (
+                {todoData?.map((item) => (
                   <TodoItem key={item.id} values={item} />
                 ))}
               </div>
