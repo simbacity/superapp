2c0e00ce40d7d8d4e88021f0da42da97726f3ff1
danieltonel <danieltonel.xyz@gmail.com>
2022-11-26 12:55:06 +0100

    fix: don't allow users to zoom in

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,10 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
-          <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          />
           <meta name="theme-color" content="#1e293c" />
           <link
             href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
