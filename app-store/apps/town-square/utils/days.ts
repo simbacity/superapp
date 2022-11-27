fb6d6c74e21db4599ceaee726ded705ff462acee
Michael Belete <it.michael.belete@gmail.com>
2022-11-28 00:02:33 +0300

    fixed date formatting issue

--- app-store/apps/town-square/utils/days.ts
+++ app-store/apps/town-square/utils/days.ts
@@ -7,22 +7,24 @@ dayjs.extend(isYesterday);
 
 export const formatDate = (date: Date | string) => {
   const parsedDate = dayjs(date);
+
   if (!parsedDate.isValid()) return "Invalid date";
 
-  const formattedTime = parsedDate.format("h:ma");
+  const hourAndMinute = parsedDate.format("h:ma");
+
   if (parsedDate.isToday()) {
-    return `Today at ${formattedTime}`;
+    return `Today at ${hourAndMinute}`;
   }
 
   if (parsedDate.isYesterday()) {
-    return `Yesterday at ${formattedTime}`;
+    return `Yesterday at ${hourAndMinute}`;
   }
 
   if (parsedDate.year() === dayjs().year()) {
-    return parsedDate.format(`MMM D at ${formattedTime}`);
+    return parsedDate.format(`MMM D`) + ` at ${hourAndMinute}`;
   }
 
-  return parsedDate.format(`MMM D YYYY, ${formattedTime}`);
+  return parsedDate.format("MMM D YYYY") + `, ${hourAndMinute}`;
 };
 
 export default dayjs;
