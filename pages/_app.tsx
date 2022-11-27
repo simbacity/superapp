f6307149f4e525377a53a98f3b368dc97badf1a6
danieltonel <danieltonel.xyz@gmail.com>
2022-11-27 19:08:50 +0100

    add installation prompt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,68 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 import { SocketProvider } from "@components/SocketProvider";
 
 import "../styles/globals.css";
 
+interface InstallAppPromptEvent extends Event {
+  prompt: () => void;
+}
+
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [installAppPromptEvent, setInstallAppPromptEvent] = useState<InstallAppPromptEvent | null>(null);
+
+  useEffect(() => {
+    if (!window) return;
+
+    const stashAddToHomeScreenPrompt = (event: Event) => {
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      setInstallAppPromptEvent(event as InstallAppPromptEvent);
+    };
+
+    window.addEventListener("beforeinstallprompt", stashAddToHomeScreenPrompt);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", stashAddToHomeScreenPrompt);
+    };
+  }, []);
+
+  const triggerInstallAppPrompt = () => {
+    if (setInstallAppPromptEvent) {
+      (installAppPromptEvent as InstallAppPromptEvent).prompt();
+      setInstallAppPromptEvent(null);
+    }
+  };
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        <SocketProvider>
-          <Component {...pageProps} />
-        </SocketProvider>
-      </SessionProvider>
-      {!IS_PRODUCTION && <ReactQueryDevtools initialIsOpen={false} />}
-    </QueryClientProvider>
+    <>
+      {installAppPromptEvent && (
+        <div className="flex w-full bg-indigo-600 text-white items-center px-4 py-4 justify-between">
+          <div>
+            <div className="text-sm font-medium">Simba City</div>
+            <div className="text-xs font-normal">Install the superapp.</div>
+          </div>
+          <a
+            onClick={() => triggerInstallAppPrompt()}
+            className="bg-white py-2 px-6 rounded-full text-indigo-600 font-semibold text-sm">
+            Install
+          </a>
+        </div>
+      )}
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider session={session}>
+          <SocketProvider>
+            <Component {...pageProps} />
+          </SocketProvider>
+        </SessionProvider>
+        {!IS_PRODUCTION && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
+    </>
   );
 }
 
