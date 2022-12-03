63f9f66a42f6c769c205b2fbbeb5d64f74177711
danieltonel <danieltonel.xyz@gmail.com>
2022-12-04 00:14:24 +0100

    message editor upgrade

--- app-store/apps/town-square/components/NewMessageForm.tsx
+++ app-store/apps/town-square/components/NewMessageForm.tsx
@@ -4,6 +4,7 @@ import {
   messageDefaultSchema,
 } from "@app-store/apps/town-square/api-contracts/message.schema";
 import { useSocket } from "@app-store/shared/hooks/useSocket";
+import { PaperAirplaneIcon } from "@heroicons/react/solid";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
@@ -44,21 +45,21 @@ export default function MessageForm({ threadId }: { threadId?: string }) {
 
   return (
     <form onSubmit={form.handleSubmit(onSubmitHandler)}>
-      <div className="fixed bottom-11 left-0 w-full">
-        <div className="flex layout px-0 lg:px-8 relative">
+      <div className="fixed bottom-11 left-0 w-full bg-slate-900 py-2">
+        <div className="flex relative max-w-6xl m-auto px-1 lg:px-8 items-center">
           <textarea
             {...form.register("content")}
-            className="textarea"
+            className="w-full border-2 border-slate-300 px-2 py-2 pr-14"
             placeholder="What's on your mind?"
-            rows={2}
+            rows={1}
             onKeyDown={handleKeyDown}
             name="content"></textarea>
-          <div className="absolute bottom-0 right-0 lg:right-8">
+          <div className="ml-2">
             <button
               type="submit"
               disabled={createMessage.isLoading}
-              className="default-button--small mb-0 mr-0">
-              Share
+              className="w-10 h-10 rounded-full bg-green-600 hover:bg-green-700 flex items-center justify-center">
+              <PaperAirplaneIcon className="w-5 h-5 rotate-90 text-white" />
             </button>
           </div>
         </div>
