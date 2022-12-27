21c3804c642f82d599d70f3766df1fb620c0cece
danieltonel <danieltonel.xyz@gmail.com>
2022-12-27 23:12:08 +0100

    make navbar sticky

--- app-store/apps/university/pages/modules/frontend-master-the-basics.tsx
+++ app-store/apps/university/pages/modules/frontend-master-the-basics.tsx
@@ -27,13 +27,19 @@ const cards = [
 export default function Module() {
   return (
     <Shell>
-      <div className="text-white pb-12">
-        <div className="mx-auto max-w-3xl px-3 md:px-0 pt-4">
+      <nav className="fixed w-full bg-gray-900 border-b border-gray-800 border-white">
+        <div className="flex items-center h-16 mx-auto max-w-3xl px-3 md:px-0">
           <Link href="/apps/university/">
-            <Button color="gray" className="pr-2">
+            <button
+              type="button"
+              className="flex items-center py-2.5 pl-1 pr-3 text-sm font-medium focus:outline-none rounded-lg focus:z-10 focus:ring-2 focus:ring-gray-700 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700">
               <ChevronLeftIcon className="h-5 w-5 mr-1" /> Back
-            </Button>
+            </button>
           </Link>
+        </div>
+      </nav>
+      <div className="text-white pt-12 pb-12">
+        <div className="mx-auto max-w-3xl px-3 md:px-0 pt-4">
           <h2 className="text-2xl font-bold pb-4 pt-8">Frontend: Master the basics</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 gap-y-8">
             {cards.map((card, index) => {
